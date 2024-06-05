39346b3eb979a2d1cf50a600319f6cc953ef05ae
Aileen Nguyen <aileenn@gmail.com>
2024-06-05 11:55:44 -0700

    saved inputs showing up

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,32 @@
 // Wraps all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering html elements
 $(document).ready(function () {
-  //shows date at top
+  //code to display the current date in the header of the page.
 
   var displayDate = document.querySelector("#currentDay");
   var currentDate = dayjs().format("dddd, MMMM D, YYYY");
 
   displayDate.textContent = currentDate;
 
+  //save tasks to local storage and display in the hour area
+
   // listener for click events on the save button.
   $(".saveBtn").on("click", function () {
     var input = $(this).siblings(".description").val();
     var hour = $(this).parent().attr("id");
     //use the id in the containing time-block as a key to save the user input in local storage.
-    localStorage.setItem(time, text);
+    localStorage.setItem(hour, input);
   });
+  console.log(localStorage);
+
+  //code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements.
+
+  function showSavedInput() {
+    $(".time-block").each(function () {
+      var blockTime = $(this).attr("id");
+      $(this).children(".description").val(localStorage.getItem(blockTime));
+    });
+  }
+  showSavedInput();
 
   //HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -26,9 +39,4 @@ $(document).ready(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
